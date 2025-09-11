12aa11dc78508ec04029df19e499fe90222c7937
Anthony <29609366+anthony-hyo@users.noreply.github.com>
2025-09-11 18:17:43 -0300

    Initial

---
+++ backend/src/main/java/com/anthhyo/stuvy/StuvyApplication.java
@@ -0,0 +1,13 @@
+package com.anthhyo.stuvy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuvyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuvyApplication.class, args);
+	}
+
+}
