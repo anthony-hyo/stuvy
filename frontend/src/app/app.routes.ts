156dee5a47014a68ef265ef8921d849bd0bab887
Anthony <29609366+anthony-hyo@users.noreply.github.com>
2025-09-12 19:16:15 -0300

    Add authentication and layout components with routing configuration

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,44 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {authGuard} from "./core/guards/auth-guard";
+import {MainLayout} from "./layouts/main-layout/main-layout";
+import {AuthLayout} from "./layouts/auth-layout/auth-layout";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: AuthLayout,
+        children: [
+            {
+                path: '',
+                loadChildren: () => import('./features/auth/auth-module').then(m => m.AuthModule)
+            }
+        ]
+    },
+
+    {
+        path: 'app',
+        component: MainLayout,
+        canActivate: [authGuard],
+        children: [
+            {
+                path: 'dashboard',
+                loadChildren: () => import('./features/dashboard/dashboard-module').then(m => m.DashboardModule)
+            },
+            {
+                path: 'materials',
+                loadChildren: () => import('./features/materials/materials-module').then(m => m.MaterialsModule)
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+        ]
+    },
+
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
---
+++ frontend/src/app/layouts/minimal-layout/minimal-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-minimal-layout',
+  imports: [],
+  templateUrl: './minimal-layout.html',
+  styleUrl: './minimal-layout.scss'
+})
+export class MinimalLayout {
+
+}
