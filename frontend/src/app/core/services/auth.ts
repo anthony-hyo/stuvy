80275fe67466d11beddc849dfc18275163348ca1
Anthony <29609366+anthony-hyo@users.noreply.github.com>
2025-09-13 01:58:41 -0300

    Implement login form with validation and authentication service integration

--- frontend/src/app/core/services/auth.ts
+++ frontend/src/app/core/services/auth.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {Router} from '@angular/router';
+import {delay, Observable, of, tap} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class Auth {
-  
+
+    private router = inject(Router);
+
+    public login(credentials: any): Observable<any> {
+        console.log(credentials);
+
+        return of({token: 'fake-jwt-for-dev-purpose'})  // Fake JWT token
+            .pipe(
+                delay(2000), // Simulate delay
+                tap(async () => await this.router.navigate(['/app/dashboard']))
+            );
+    }
 }
--- frontend/src/app/features/auth/components/login/login.ts
+++ frontend/src/app/features/auth/components/login/login.ts
@@ -1,15 +1,59 @@
-import { Component } from '@angular/core';
-import {CommonModule} from "@angular/common";
+import {Component, inject} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCardModule} from '@angular/material/card';
+import {Auth} from "../../../../core/services/auth";
+
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [
-    CommonModule
-  ],
-  templateUrl: './login.html',
-  styleUrl: './login.scss'
+    selector: 'app-ddd',
+    imports: [
+        MatInputModule,
+        MatButtonModule,
+        MatSelectModule,
+        MatRadioModule,
+        MatCardModule,
+        ReactiveFormsModule
+    ],
+    templateUrl: './login.html',
+    styleUrl: './login.scss',
 })
 export class Login {
+
+    private readonly formBuilder = inject(FormBuilder);
+    private readonly authService = inject(Auth)
+
+    public readonly loginForm = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+
+    public get email() {
+        return this.loginForm.get('email')!;
+    }
+
+    public get password() {
+        return this.loginForm.get('password')!;
+    }
+
+    protected onSubmit(): void {
+        if (this.loginForm.invalid) {
+            return;
+        }
+        
+        this.authService
+            .login(this.loginForm.value)
+            .subscribe({
+                next: result => {
+                    console.log(result);
+                },
+                error: err => {
+                    console.error('Login error', err);
+                }
+            })
+    }
 
 }
